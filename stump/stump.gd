e52d8a04a4c4bab5e890b075c2e44754911f841d
Jadi Miller <jadilmiller@gmail.com>
2024-05-24 14:36:38 -0400

    Change stump_state from enum to bool

--- stump/stump.gd
+++ stump/stump.gd
@@ -10,46 +10,28 @@ enum StumpState {
 	EMPTY
 	}
 
-var stump_state:
-	set(state):
-		if state == StumpState.EMPTY:
-			set_texture()
-		if state == StumpState.FULL:
-			set_texture()
-		stump_state = state
+var _is_empty : bool
+
 
 var _pampered_frog : Frog
 
 
 func _ready() -> void:
-	stump_state = StumpState.EMPTY
-	set_texture()
-
-
-func set_texture() -> void:
-	match stump_state:
-		StumpState.FULL:
-			return
-		StumpState.EMPTY:
-			return
+	_is_empty = true
 
 
 func _on_body_entered(body) -> void:
-	if stump_state == StumpState.EMPTY and body is Frog:
-		stump_state = StumpState.FULL
+	if _is_empty == true and body is Frog:
+		_is_empty = false
 
 		emit_signal("entered", body, _get_marker_position())
 
 		_pampered_frog = body
-		print("Entered:" + str(_pampered_frog))
-		print(stump_state)
 
 
 func _on_body_exited(body) -> void:
 	if body == _pampered_frog:
-		stump_state = StumpState.EMPTY
-		print("Exited:" + str(_pampered_frog))
-		print(stump_state)
+		_is_empty = true
 
 
 func _get_marker_position() -> Vector2:
